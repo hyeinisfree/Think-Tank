11ce53e1799d5a9a26c57a735f8b7cada7889cb3
sio2whocodes <fltcy2039@naver.com>
2023-11-18 20:40:06 +0900

    Feat: User, Star Repository 생성

--- src/main/java/com/ttteam/thinktank/repository/StarRepository.java
+++ src/main/java/com/ttteam/thinktank/repository/StarRepository.java
@@ -3,6 +3,6 @@ package com.ttteam.thinktank.repository;
 import com.ttteam.thinktank.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface StarRepository extends JpaRepository<User, Long> {
 
 }
