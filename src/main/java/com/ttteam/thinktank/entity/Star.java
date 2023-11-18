de949701eab868b0dbdfd51f39ded9f2191e6ef7
sio2whocodes <fltcy2039@naver.com>
2023-11-18 20:38:55 +0900

    Feat: User, Star entity 작성

--- src/main/java/com/ttteam/thinktank/entity/Star.java
+++ src/main/java/com/ttteam/thinktank/entity/Star.java
@@ -2,15 +2,16 @@ package com.ttteam.thinktank.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.NonNull;
 import lombok.experimental.SuperBuilder;
-import org.antlr.v4.runtime.misc.NotNull;
 import org.hibernate.annotations.DynamicInsert;
 
 @Getter
@@ -19,22 +20,19 @@ import org.hibernate.annotations.DynamicInsert;
 @AllArgsConstructor
 @DynamicInsert
 @Entity
-public class User extends BaseTimeEntity {
+public class Star extends BaseTimeEntity{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(unique = true, name = "naver_id")
-    private String naverId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
 
-    private String name;
+    private Integer group;
 
-    private String email;
+    private String title;
 
-    private String age;
-
-    private String gender;
-
-    private String birthday;
-
+    @Column(columnDefinition = "TEXT")
+    private String content;
 }
